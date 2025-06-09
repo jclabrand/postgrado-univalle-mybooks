8659337acf2e5d60267b864e0ca6c22b715ba2a2
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-06-09 01:25:25 -0400

    fix: auth context with user data

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,23 +1,38 @@
 import React, { createContext, useState, useContext } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { auth } from '../config/firebase';
+import { db } from '../config/firebase';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-  const login = (/* userData */) => {
-    // setUser(userData);
-    setIsAuthenticated(true);
+  const login = async () => {
+    try {
+      const docRef = doc(db, 'usuarios', auth.currentUser.uid);
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.exists()) {
+        setUser({
+          ...docSnapshot.data(),
+          uid: auth.currentUser.uid,
+        });
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      alert('No se pudo cargar el perfil. Inténtalo de nuevo: ' + error.message);
+    }
   };
 
   const logout = () => {
-    // setUser(null);
+    setUser(null);
     setIsAuthenticated(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout /*, user */ }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, user }}>
       {children}
     </AuthContext.Provider>
   );
