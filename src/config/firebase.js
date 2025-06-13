fd8f70ad77d98c5d5058fdda656ddf64f5436477
Juan Carlos Labrandero <jcharlylc@gmail.com>
2025-06-13 00:29:06 -0400

    chore: firebase storage

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getStorage, connectStorageEmulator } from 'firebase/storage'; // <-- Importa storage
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLW7tzr03dg5wtOXkuwuvVHEQrcLd9N8A",
@@ -10,13 +11,15 @@ const firebaseConfig = {
   messagingSenderId: "1036110369361",
   appId: "1:1036110369361:web:69b2b36e6edc16a7709e70"
 };
-const localEmulatorIp = '192.168.124.129';
+const localEmulatorIp = '192.168.100.6';
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 connectAuthEmulator(auth, `http://${localEmulatorIp}:9099`, { disableWarnings: true });
 connectFirestoreEmulator(db, localEmulatorIp, 9080);
+connectStorageEmulator(storage, localEmulatorIp, 9199);
 
-export { auth, db };
+export { auth, db, storage };
